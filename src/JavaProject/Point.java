4cdb21c65f3ce22618f8dee96764225390a0178b
krzurb <101674041+krzurb@users.noreply.github.com>
2022-06-21 23:24:30 +0200

    Add files via upload

---
+++ src/JavaProject/Point.java
@@ -0,0 +1,76 @@
+package JavaProject;
+
+import java.awt.Color;
+
+public class Point {
+	int x;
+	int y;
+	Color kolor;
+	
+	public void setx(int x) 
+	{
+		this.x=x;
+	}
+	public void sety(int y) 
+	{
+		this.y=y;
+	}
+	public void setkolor(float x, float y, float z) 
+	{
+		Color kolo =new Color(hsvToRgb(x,y,z)[0],hsvToRgb(x,y,z)[1],hsvToRgb(x,y,z)[2]);
+		
+		this.kolor=kolo;
+	}
+	public static int[] hsvToRgb(float hue, float saturation, float brightness) {
+
+		int r = 0, g = 0, b = 0;
+        if (saturation == 0) {
+            r = g = b = (int) (brightness * 255.0f + 0.5f);
+        } else {
+            float h = (hue - (float)Math.floor(hue)) * 6.0f;
+            float f = h - (float)java.lang.Math.floor(h);
+            float p = brightness * (1.0f - saturation);
+            float q = brightness * (1.0f - saturation * f);
+            float t = brightness * (1.0f - (saturation * (1.0f - f)));
+            switch ((int) h) {
+            case 0:
+                r = (int) (brightness * 255.0f + 0.5f);
+                g = (int) (t * 255.0f + 0.5f);
+                b = (int) (p * 255.0f + 0.5f);
+                break;
+            case 1:
+                r = (int) (q * 255.0f + 0.5f);
+                g = (int) (brightness * 255.0f + 0.5f);
+                b = (int) (p * 255.0f + 0.5f);
+                break;
+            case 2:
+                r = (int) (p * 255.0f + 0.5f);
+                g = (int) (brightness * 255.0f + 0.5f);
+                b = (int) (t * 255.0f + 0.5f);
+                break;
+            case 3:
+                r = (int) (p * 255.0f + 0.5f);
+                g = (int) (q * 255.0f + 0.5f);
+                b = (int) (brightness * 255.0f + 0.5f);
+                break;
+            case 4:
+                r = (int) (t * 255.0f + 0.5f);
+                g = (int) (p * 255.0f + 0.5f);
+                b = (int) (brightness * 255.0f + 0.5f);
+                break;
+            case 5:
+                r = (int) (brightness * 255.0f + 0.5f);
+                g = (int) (p * 255.0f + 0.5f);
+                b = (int) (q * 255.0f + 0.5f);
+                break;
+            }
+        }
+
+        int[] rgb = new int[3];
+        rgb[0] = r;
+        rgb[1] = g;
+        rgb[2] = b;
+
+        return rgb;
+    }
+}
